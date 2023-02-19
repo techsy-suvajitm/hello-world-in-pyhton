e693d77011fbd28bc05484f7b0ce3ef762edd771
suvajitmixn <suvajit.majumder@placeexchange.com>
2023-02-19 17:00:28 +0530

    inserted bad code

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,24 @@
-print('Hello World')
-def func_x(num):
-    if num > 10:
-        return True
-    else:
-        return False
-print(func_x(10))
-print('hello bye')
+######################
+# Positional Arguments
+######################
+
+param_args = [1, 2, 3]
+param_kwargs = {'x': 1, 'y': 2}
+
+def func(a, b=1):
+    print(a, b)
+
+def positional_unlimited(a, b=1, *args):
+    print(a, b, *args)
+
+func(1)
+func(1, 42)
+func(1, 2, 3)  # Noncompliant. Too many positional arguments
+func()  # Noncompliant. Missing positional argument for "a"
+
+positional_unlimited(1, 2, 3, 4, 5)
+
+def positional_limited(a, *, b=2):
+    print(a, b)
+
+positional_limited(1, 2)  # Noncompliant. Too many positional arguments
